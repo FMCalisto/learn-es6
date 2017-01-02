0af790bf2585f674f35e9f2d80c900d878179e53
FMCalisto <francisco_mcalisto@hotmail.com>
2017-01-02 21:44:24 +0000

    [UPDATE] Arros Functions Delivery Boy

--- get-started/app.js
+++ get-started/app.js
@@ -1,5 +1,21 @@
 var deliveryBoy = {
-    name: "",
+    
+    name: "John",
+
+    handleMessage: function (message, handle) {
+        hendler(message);
+    },
+
+    receive: function() {
+        var that = this;
+
+        this.handleMessage("Message: ", function (message) {
+            that.name; //get the proper name
+
+            console.log(message + that.name);
+        })
+    }
+
 }
 
 deliveryBoy.receive();
