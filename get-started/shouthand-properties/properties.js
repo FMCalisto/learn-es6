3d81ed75d143a37fb4c4a4519061f812639c2ff2
FMCalisto <francisco_mcalisto@hotmail.com>
2017-01-04 22:02:07 +0000

    [CREATE] Shorthand Properties Source

---
+++ get-started/shouthand-properties/properties.js
@@ -0,0 +1,11 @@
+let firstName = "John";
+let lastName = "Lindquist";
+
+let person = { firstName, lastName }
+
+console.log(person);
+
+let mascot = "Moose";
+let team = { person, mascot };
+
+console.log(team);
