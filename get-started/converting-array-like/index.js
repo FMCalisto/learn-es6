044a2eaa2f8816365044e4bc616274dec2a34465
FMCalisto <francisco_mcalisto@hotmail.com>
2017-01-18 18:56:08 +0000

    [ADD] Index Converting Array-Like

---
+++ get-started/converting-array-like/index.js
@@ -0,0 +1,8 @@
+const products =
+  Array.from(document.querySelectorAll('.product'));
+
+products
+  .filter(product => parseFloat(product.innerHTML) < 10)
+  .forEach(product => product.style.color = 'red');
+
+console.log(products);
