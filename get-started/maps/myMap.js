b045e27763304ffc0cc3c58c658d66753f600570
FMCalisto <francisco_mcalisto@hotmail.com>
2017-01-22 00:26:36 +0000

    [UPDATE] Map Iterator

--- get-started/maps/myMap.js
+++ get-started/maps/myMap.js
@@ -12,4 +12,23 @@ has()
 myMap.set('foo', 'bar');
 myMap.set('hello', 'world');
 
-console.log(myMap.get('foo'));
+// console.log(myMap.get('foo'));
+// console.log(myMap.size);
+// console.log(myMap.has('foo'));
+
+//Iterators
+//keys()
+//entries()
+//values
+
+// for (var key of myMap.keys()) {
+//   console.log(key);
+// }
+
+// for (var value of myMap.values()) {
+//   console.log(value);
+// }
+
+for (var [key, value] of myMap.entries()) {
+  console.log(key + ' = ' + value);
+}
